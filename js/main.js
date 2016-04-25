3142d5614e3ba56af362767d35d2f829ab949106
zhimmat <zeyad.himmat@live.com>
2016-04-25 12:22:55 -0400

    changed spacing

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-$('ul').localScroll();
-
 var $body = $('body');
 
 var $banner = $('.banner');
@@ -11,45 +9,46 @@ var $bottom = $('.bottom');
 var $btnMove = $('.btn-move');
 var $ambulance = $('.ambulance');
 
+$('ul').localScroll();
+
 $banner.waypoint(function (direction) {
   if (direction == 'down') {
     $banner.addClass('js-banner-animate');
   } else {
-  $banner.removeClass('js-banner-animate');
-}
-}, { offset: 'bottom-in-view' });
-
+    $banner.removeClass('js-banner-animate');
+  }
+}, {offset: 'bottom-in-view'});
 $whitenurses.waypoint(function (direction) {
   if (direction == 'down') {
     $whitenurses.addClass('js-whitenurses-animate');
   } else {
-  $whitenurses.removeClass('js-whitenurses-animate');
-}
-}, { offset: 'bottom-in-view' });
+    $whitenurses.removeClass('js-whitenurses-animate');
+  }
+}, {offset: 'bottom-in-view'});
 
 $bluenurses.waypoint(function (direction) {
   if (direction == 'down') {
     $bluenurses.addClass('js-bluenurses-animate');
   } else {
-  $bluenurses.removeClass('js-bluenurses-animate');
-}
-}, { offset: 'bottom-in-view' });
+    $bluenurses.removeClass('js-bluenurses-animate');
+  }
+}, {offset: 'bottom-in-view'});
 
 $icons.waypoint(function (direction) {
   if (direction == 'down') {
     $icons.addClass('js-icons-animate');
   } else {
-  $icons.removeClass('js-icons-animate');
-}
-}, { offset: 'bottom-in-view' });
+    $icons.removeClass('js-icons-animate');
+  }
+}, {offset: 'bottom-in-view'});
 
 $bottom.waypoint(function (direction) {
   if (direction == 'down') {
     $bottom.addClass('js-bottom-animate');
   } else {
-  $bottom.removeClass('js-bottom-animate');
-}
-}, { offset: 'bottom-in-view' });
+    $bottom.removeClass('js-bottom-animate');
+  }
+}, {offset: 'bottom-in-view'});
 
 $btnMove.on('click', function () {
   $ambulance.toggleClass('btn-slide');
